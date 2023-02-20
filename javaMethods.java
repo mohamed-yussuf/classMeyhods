5176a19529d2bf2255059f09f13a07da4ab2edf2
mohamed-yussuf <125584764+mohamed-yussuf@users.noreply.github.com>
2023-02-20 19:20:38 +0300

    Add files via upload
    my first commit

---
+++ javaMethods.java
@@ -0,0 +1,28 @@
+public class javaMethods {
+
+    static void moha() {
+
+        System.out.println("My name is Mohammed");
+    }
+
+    static void saalim() {
+        System.out.println(" My name is Saalim");
+    }
+
+    static void Area() {
+        int s = 13;
+        int Area_square = s * s;
+        System.out.println("Area of square " + Area_square);
+    }
+
+    public static void main(String[] args) {
+
+
+//        for (int i = 0; i <= 10; i++) {
+//            System.out.println("the person is " + i);
+//            moha();
+//            saalim();
+//        }
+        Area();
+    }
+}
